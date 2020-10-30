9d3ccebc2de1ac15dc972b466ead6467302e2adf
Dylan Aigner <dylanaigner@gmail.com>
2020-10-30 09:26:44 +0200

    Changed project

---
+++ PetHaven/DAL/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using PetHaven.Models;
+using System.Data.Entity;
+
+namespace PetHaven.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductImageMapping> ProductImageMappings { get; set; }
+        public DbSet<BasketLine> BasketLines { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
---
+++ PetHaven/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetHaven.Startup))]
+namespace PetHaven
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
