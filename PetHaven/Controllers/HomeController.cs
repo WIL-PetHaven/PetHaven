6e16710c60583094c663501acd036dd41c0567a3
Costa <calvincosta1@gmail.com>
2020-11-11 19:20:59 +0200

    Contact form UI complete.

--- PetHaven/Controllers/HomeController.cs
+++ PetHaven/Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+
 
 namespace PetHaven.Controllers
 {
@@ -23,8 +27,27 @@ namespace PetHaven.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        public ActionResult Email()
+        {
+            Execute();
+
+            return View($"Email sent");
+        }
+
+        static void Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg).Result;
         }
     }
 }
