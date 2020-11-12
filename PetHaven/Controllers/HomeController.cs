4e4307da462a4713b206ff920a15c7ecd236a44f
Costa <calvincosta1@gmail.com>
2020-11-12 13:06:12 +0200

    Test.

--- PetHaven/Controllers/HomeController.cs
+++ PetHaven/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-
+using PetHaven.Models;
 
 namespace PetHaven.Controllers
 {
@@ -35,6 +35,31 @@ namespace PetHaven.Controllers
             Execute();
 
             return View($"Email sent");
+        }
+
+        // Method used to post the Contact page
+        [HttpPost]
+        public async Task Email(ContactForm model)
+        {
+            // If the model state is valid (all fields have passed validation), then send a message to admin@testsetup.net
+            if (ModelState.IsValid)
+            {
+                // Gets a new GUID for the contact form
+                guid = Guid.NewGuid();
+                // Sends the email with all required information
+                await _emailSender.SendEmailAsync("admin@testsetup.net", "Reference Number: "
+                                                                         + guid, "<h2>Email: " + model.Email + "</h2>"
+                                                                                 + "<br>" + "<h2>Message</h2>" +
+                                                                                 "<p>" + model.Message.Replace("\n", "<br>") + "</p>");
+                await _emailSender.SendEmailAsync(model.Email, "Reference Number: "
+                                                                         + guid, "<h2>Email: " + model.Email + "</h2>"
+                                                                                 + "<br>" + "<h2>Message</h2>" +
+                                                                                 "<p>" + model.Message.Replace("\n", "<br>") + "</p>");
+                // Return confirmation page
+                return RedirectToAction("MessageSubmitted");
+            }
+            // Returns the contact page
+            return About();
         }
 
         static void Execute()
