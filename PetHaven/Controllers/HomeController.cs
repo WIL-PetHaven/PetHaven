a807e50048ef305180a9d443575e8f3253ec4312
Costa <calvincosta1@gmail.com>
2020-11-13 19:46:14 +0200

    Emailing service intergrated.

--- PetHaven/Controllers/HomeController.cs
+++ PetHaven/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using PetHaven.Models;
+using System.Diagnostics;
 
 namespace PetHaven.Controllers
 {
@@ -30,24 +31,35 @@ namespace PetHaven.Controllers
             return View();
         }
 
-        public ActionResult Email()
+        // Method used to post the Contact page
+        [HttpPost]
+        public async Task<ActionResult> Contact(ContactForm model)
         {
-            Execute();
+            await Execute(model);
 
-            return View($"Email sent");
-        }       
+            // Return confirmation page
+            return View();
+        } 
 
-        static void Execute()
+        static async Task Execute(ContactForm model)
         {
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = client.SendEmailAsync(msg).Result;
+            var from = new EmailAddress("ngo.pethaven@gmail.com", "PetHaven");
+
+            var subject1 = "Message Recieved";
+            var to1 = new EmailAddress(model.Email, model.Name);
+            var plainTextContent1 = model.Message;
+            var htmlContent1 = model.Message;
+            var msg1 = MailHelper.CreateSingleEmail(from, to1, subject1, plainTextContent1, htmlContent1);
+            var response1 = await client.SendEmailAsync(msg1);
+
+            var subject2 = "Contact Form Message";
+            var to2 = new EmailAddress("ngo.pethaven@gmail.com", "PetHaven");
+            var plainTextContent2 = model.Message;
+            var htmlContent2 = model.Message;
+            var msg2 = MailHelper.CreateSingleEmail(from, to2, subject2, plainTextContent2, htmlContent2);
+            var response2 = await client.SendEmailAsync(msg2);
         }
     }
 }
--- PetHaven/Models/ContactForm.cs
+++ PetHaven/Models/ContactForm.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace PetHaven.Models
 {
     public class ContactForm
     {
         [Required]
-        [RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a name made up of letters only")]
+        [MaxLength(50)]
+        //[RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a name made up of letters only")]
         public string Name { get; set; }
         [Required]
+        [MaxLength(50)]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
+        [MaxLength(15)]
         [Phone]
+        [Display(Name="Phone No.")]
         public string Phone { get; set; }
         [Required]
-        [RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a message made up of letters and numbers only")]
+        [MaxLength(250)]
+        //[RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a message made up of letters and numbers only")]
         public string Message { get; set; }
     }
 }
