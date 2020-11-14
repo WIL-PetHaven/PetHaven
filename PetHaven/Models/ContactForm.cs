b6208b1e3aecba10b745d7f86a505ce7da72fd28
Costa <calvincosta1@gmail.com>
2020-11-14 10:30:13 +0200

    Final commit for validation messages.

--- PetHaven/Models/ContactForm.cs
+++ PetHaven/Models/ContactForm.cs
@@ -9,19 +9,19 @@ namespace PetHaven.Models
 {
     public class ContactForm
     {
-        [Required]
+        [Required(ErrorMessage = "Please make sure to enter in your full name.")]
         [MaxLength(50)]
         //[RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a name made up of letters only")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please make sure to enter in your e-mail address.")]
         [MaxLength(50)]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please make sure to enter in your phone number.")]
         [MaxLength(15)]
         [Phone]
         public string Phone { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please make sure to enter in your message.")]
         [MaxLength(250)]
         //[RegularExpression(@"^[,;a-zA-Z0-9'-'\s:-.#&!@$?%*()/|""+-~{}”’<>]$", ErrorMessage = "Please enter a message made up of letters and numbers only")]
         public string Message { get; set; }
