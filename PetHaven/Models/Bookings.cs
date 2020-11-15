74330707fe2901898f1d13127e67f69f89305002
Dylan Aigner <dylanaigner@gmail.com>
2020-11-15 15:09:21 +0200

    booking feature complete - ui fixes needed

--- PetHaven/Models/Bookings.cs
+++ PetHaven/Models/Bookings.cs
@@ -12,16 +12,16 @@ namespace PetHaven.Models
         [Display(Name = "User")]
         public string UserID { get; set; }
 
-        [Display(Name = "Deliver to")]
+        [Display(Name = "Booking By")]
         public string DeliveryName { get; set; }
 
-        [Display(Name = "Delivery Address")]
+        [Display(Name = "User Address")]
         public Address DeliveryAddress { get; set; }
 
         [Display(Name = "Animal Jake")]
         public string AnimalName { get; set; }
 
-        [Display(Name = "Time of Order")]
+        [Display(Name = "Booking Date")]
         public DateTime DateCreated { get; set; }
 
         public List<BookingsLine> BookingsLines { get; set; }
