5889303caf5fcc97cf79bc42b098320821d6f591
Dylan Aigner <dylanaigner@gmail.com>
2020-11-27 12:16:43 +0200

    small improvements to ui

---
+++ PetHaven/Migrations/202011271005459_AddBookings4.cs
@@ -0,0 +1,16 @@
+namespace PetHaven.Migrations.StoreConfiguration
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBookings4 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
